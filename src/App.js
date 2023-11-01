ec969aaa49b3e585e8646df2764cdfa22bd1a255
Anthony <anthonyoutub@gmail.com>
2023-11-01 11:18:55 +0100

    optimize router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import '@/App.css';
 
-import Service from '@/pages/Service';
-import Home from '@/pages/Home.js'
-import Contact from '@/pages/Contact';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import PublicRouter from '@/pages/Public/PublicRouter';
+
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
-      <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/home" element={<Contact />} />
-      </Routes>
+      <PublicRouter />
     </BrowserRouter>
     </div>
   );
--- src/pages/Public/Contact.js
+++ src/pages/Public/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Contact = () => {
+export const Contact = () => {
   return (
     <div>Contact Works !</div>
   )
